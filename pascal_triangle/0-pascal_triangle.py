fa25e0aec68e175cd37d443b03416b18768af3fd
Bridget Chimdinma <139658311+preshbridge@users.noreply.github.com>
2024-06-15 07:18:14 +0100

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Pascal's Triangle."""
+"""Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal triangle"""
 
 
-def pascal_triangle(a):
-    """Generate Pascal's Triangle up to row 'a'"""
-    if a <= 0:
+def pascal_triangle(n):
+    """function that returns a list of lists of integers"""
+    if n <= 0:
         return []
     triangle = [[1]]
-    for i in range(1, a):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
